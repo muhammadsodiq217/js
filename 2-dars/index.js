fd59ee74aa3d8c70afc0b6434d02fdbecdb0ed14
--replace-all <ilyosbek.351.60.96>
2025-05-17 05:08:30 +0500

    js

---
+++ 2-dars/index.js
@@ -0,0 +1,59 @@
+// let str = "                  -12.98n43";
+
+// console.log(parseInt(str));
+// console.log(parseFloat(str));
+// // console.log(+str);
+// let num = 82;
+
+// console.log(`${"ddsds"}`);
+
+// console.log("8n" * 2);
+// console.log(2 * "8");
+
+// console.log("8s" - 2);
+// console.log(2 - "8");
+
+// console.log("8" / 2);
+// console.log(2 / "8");
+
+// false , true
+// false qiymatlar - 0, -0, null, undefined,""
+
+// console.log(Boolean());
+
+// console.log("BB" < "Ba");
+
+// let a = 7;
+
+// console.log(--a);
+// console.log(a);
+
+// console.log(a-- - 3);
+
+// // console.log(a);
+
+// let num = 42;
+// let num1 = 42;
+// let num2 = 42;
+// let num3 = 42;
+
+// num += 7; // num = num + 7
+// num1 -= 7; // num = num - 7
+// num2 /= 7; // num = num / 7
+// num3 *= 7; // num = num * 7
+
+// console.log(num);
+// console.log(num1);
+// console.log(num2);
+// console.log(num3);
+
+// console.log(9 ** 3);
+// // console.log(11 % 3);
+// console.log(3 && 6 && 6);
+// console.log(0 || undefined || 0);
+
+// console.log(7 === 2);
+// console.log(7 == 2);
+
+// console.log(9 !== "9");
+// console.log(9 != "9");
---
+++ 2-dars/node.md
@@ -0,0 +1,41 @@
+# Type conversation
+
+String to number
+Number()//belgi qatnashsa NaN qaytaradi
+parseint()//belgigacha bo'lgan sonlarni oladi kasr qismini olmaydi
+parsefloat()//belgigacha bo'lgan sonlarni oladi kasr qismini oladi
+
+Number to string
+Srtirg()
+`${}`
+
+string + number = string
+number + string = string
+string _ number = number
+number _ string = number
+string / number = number
+number / string = number
+string - number = number
+number - string = number
+
+++, --// increment, decrement - +1, -1
+
+&&, || // or, and
+
+Or - trueni qidiradi topolmasa oxirgi qoymatni oladi
+And - falseni qidiradi topolmasa oxirgi qoymatni oladi
+
+!/ true qiymatni false false qiymatni true deb qaytaradi
+
+// num += 7; // num = num + 7
+// num1 -= 7; // num = num - 7
+// num2 /= 7; // num = num / 7
+// num3 _= 7; // num = num _ 7
+
+== - data typini tekshirmaydi
+=== - data typeni tekshiradi
+!= - == teng false bersa true aks holda false qaytaradi
+!== - === teng false bersa true aks holda false qaytaradi
+
+A > a // false
+// false qiymatlar - 0, -0, null, undefined,""
